fbf42f74ae380a1f91f559280779396a92a27e7d
Anil Pathuri <anil.pathuri@netcracker.com>
2023-12-16 04:15:18 +0530

    commit message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: LoginComponent
+  },
+  {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: MainComponent
   },
   {
     path: 'overview',
