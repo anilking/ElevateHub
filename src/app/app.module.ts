5d9e03a7a4066819ff431472c96d507880aca265
Anil Pathuri <anil.pathuri@netcracker.com>
2023-12-15 23:38:10 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import {UMSideNavigationBarModule} from "@netcracker/um-ng2/components/side-navigation-bar";
+import {UxHeaderModule} from "@netcracker/ux-ng2/header";
+import {UxMultipleButtonModule} from "@netcracker/ux-ng2/multiple-button";
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+
+// const uxModules: Type<any>[] = [
+//   UxOverlayModule,
+//   UxLinearDotsLoaderModule,
+//   UxLinkButtonModule,
+//   UxLoaderModule
+// ];
+
+const umModules: Type<any>[] = [
+  UMSideNavigationBarModule,
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    UxHeaderModule,
+    UxMultipleButtonModule,
+    
+    ...umModules,
   ],
   providers: [],
   bootstrap: [AppComponent]
